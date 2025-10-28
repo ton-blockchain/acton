fc7a9fe48dc1f7d48fe555ed8ef3aee8a98cf270
i582 <51853996+i582@users.noreply.github.com>
2025-10-28 13:04:37 +0400

    remove third-party dap crate

--- crates/dap/src/prelude.rs
+++
@@ -1,9 +0,0 @@
-#[doc(hidden)]
-pub use crate::{
-    events::{self, Event},
-    requests::{self, Command, Request},
-    responses::{self, Response, ResponseBody},
-    reverse_requests::{ReverseCommand, ReverseRequest},
-    server::Server,
-    types,
-};
