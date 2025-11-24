05493467bd14728bf29565d2c533afaae6cbced0
i582 <51853996+i582@users.noreply.github.com>
2025-11-24 12:48:07 +0400

    add more test for step over for function calls

--- tests/debugging/tests/functions.rs
+++ tests/debugging/tests/functions.rs
@@ -112,6 +112,129 @@ fun main() {
 }
 
 #[test]
+fn test_inline_function_call_step_over_with_nested_calls() -> anyhow::Result<()> {
+    let code = r#"
+global foo: int;
+
+@inline
+fun dump(a: int) {
+    debug.print(a);
+}
+
+fun my_sum(a: int, b: int): int {
+    dump(a);
+    dump(b);
+    return a + b;
+}
+
+fun main() {
+    foo = 300;
+    val goo = my_sum(foo, foo);
+    return foo + goo;
+}
+"#;
+
+    let session = DebugBuilder::new("debug-callback").code(code).build();
+
+    let mut client = session.start();
+
+    let result = client.execute(|executor| {
+        executor.step_over()?;
+        executor.step_over()?;
+        Ok(())
+    })?;
+
+    let debug_output = DebugTestOutput::new(result);
+    debug_output.assert_trace_snapshot_matches(
+        "debugging/snapshots/function_call/inline/step_over_with_nested_calls.trace.txt",
+    );
+
+    Ok(())
+}
+
+#[test]
+fn test_inline_function_call_step_over_with_nested_inline_ref_calls() -> anyhow::Result<()> {
+    let code = r#"
+global foo: int;
+
+@inline_ref
+fun dump(a: int) {
+    debug.print(a);
+}
+
+fun my_sum(a: int, b: int): int {
+    dump(a);
+    dump(b);
+    return a + b;
+}
+
+fun main() {
+    foo = 300;
+    val goo = my_sum(foo, foo);
+    return foo + goo;
+}
+"#;
+
+    let session = DebugBuilder::new("debug-callback").code(code).build();
+
+    let mut client = session.start();
+
+    let result = client.execute(|executor| {
+        executor.step_over()?;
+        executor.step_over()?;
+        Ok(())
+    })?;
+
+    let debug_output = DebugTestOutput::new(result);
+    debug_output.assert_trace_snapshot_matches(
+        "debugging/snapshots/function_call/inline/step_over_with_nested_inline_ref_calls.trace.txt",
+    );
+
+    Ok(())
+}
+
+#[test]
+fn test_inline_function_call_step_over_with_nested_no_inline_calls() -> anyhow::Result<()> {
+    let code = r#"
+global foo: int;
+
+@noinline
+fun dump(a: int) {
+    debug.print(a);
+}
+
+fun my_sum(a: int, b: int): int {
+    dump(a);
+    dump(b);
+    return a + b;
+}
+
+fun main() {
+    foo = 300;
+    val goo = my_sum(foo, foo);
+    return foo + goo;
+}
+"#;
+
+    let session = DebugBuilder::new("debug-callback").code(code).build();
+
+    let mut client = session.start();
+
+    let result = client.execute(|executor| {
+        executor.step_over()?;
+        executor.step_over()?;
+        Ok(())
+    })?;
+
+    let debug_output = DebugTestOutput::new(result);
+    debug_output.assert_trace_snapshot_matches(
+        "debugging/snapshots/function_call/inline/step_over_with_nested_noinline_calls.trace.txt",
+    );
+
+    Ok(())
+}
+
+#[test]
 fn test_ref_inline_function_call_step_in() -> anyhow::Result<()> {
     let code = r#"
 global foo: int;
@@ -297,6 +420,44 @@ fun main() {
     let debug_output = DebugTestOutput::new(result);
     debug_output.assert_trace_snapshot_matches(
         "debugging/snapshots/function_call/no_inline/step_over.trace.txt",
+    );
+
+    Ok(())
+}
+
+#[test]
+fn test_noinline_recursive_function_call_step_over() -> anyhow::Result<()> {
+    let code = r#"
+global foo: int;
+
+@method_id(123)
+fun fib(a: int): int {
+    if (a <= 1) {
+        return a
+    }
+    return fib(a - 1) + fib(a - 2);
+}
+
+fun main() {
+    foo = 5;
+    val res = fib(foo);
+    return res + foo;
+}
+"#;
+
+    let session = DebugBuilder::new("debug-callback").code(code).build();
+
+    let mut client = session.start();
+
+    let result = client.execute(|executor| {
+        executor.step_over()?;
+        executor.step_over()?;
+        Ok(())
+    })?;
+
+    let debug_output = DebugTestOutput::new(result);
+    debug_output.assert_trace_snapshot_matches(
+        "debugging/snapshots/function_call/no_inline/recursive_step_over.trace.txt",
     );
 
     Ok(())
