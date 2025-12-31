5c2c7aaa9270bcb02d7221fa8a570b9c54fa101c
Danil Ovchinnikov <danil42worker@gmail.com>
2025-12-31 19:22:00 +0300

    rename

--- crates/tolkc/build.rs
+++ crates/tolkc/build.rs
@@ -4,16 +4,10 @@ use std::path::Path;
 fn main() {
     let workspace_root = Path::new(&env::var("CARGO_MANIFEST_DIR").unwrap())
         .join("..")
-        .join("..")
-        .join("objs");
+        .join("..");
+    let objc_dir = workspace_root.join("objs");
 
-    println!(
-        "cargo:rustc-link-search=native={}",
-        workspace_root.display()
-    );
-    println!(
-        "cargo:rerun-if-changed={}/libtolk.a",
-        workspace_root.display()
-    );
+    println!("cargo:rustc-link-search=native={}", objc_dir.display());
+    println!("cargo:rerun-if-changed={}/libtolk.a", objc_dir.display());
     println!("cargo:rustc-link-lib=static=tolk");
 }
