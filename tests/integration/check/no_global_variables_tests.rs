34ec2f243739b5a10c9a9fb95d185871ea17e871
Danil Ovchinnikov <danil42worker@gmail.com>
2026-03-01 01:46:54 +0300

    feat(check): add inspection for global variables

---
+++ tests/integration/check/no_global_variables_tests.rs
@@ -0,0 +1,52 @@
+use crate::integration::check::run_simple_test;
+use function_name::named;
+
+#[test]
+#[named]
+fn test_check_no_global_variables_reports_global_declaration() {
+    run_simple_test(
+        "no_global_variables",
+        r#"
+            global counter: int
+
+            fun main() {
+                 counter = 100;
+            }
+        "#,
+        function_name!(),
+    )
+}
+
+#[test]
+#[named]
+fn test_check_no_global_variables_ignores_local_variables() {
+    run_simple_test(
+        "no_global_variables",
+        r#"
+            fun main() {
+                val counter = 1;
+                val total = counter + 41;
+                total;
+            }
+        "#,
+        function_name!(),
+    )
+}
+
+#[test]
+#[named]
+fn test_check_no_global_variables_reports_multiple_global_declarations() {
+    run_simple_test(
+        "no_global_variables",
+        r#"
+            global counter: int
+            global owner: address
+
+            fun main() {
+                counter;
+                owner;
+            }
+        "#,
+        function_name!(),
+    )
+}
