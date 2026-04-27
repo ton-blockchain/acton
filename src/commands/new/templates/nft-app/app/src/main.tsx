0cc023fe4fe74c3131ae7cc00f5a0aa76f3550b8
Petr Makhnev <51853996+i582@users.noreply.github.com>
2026-04-27 20:21:20 +0400

    more fixes

--- src/commands/new/templates/nft-app/app/src/main.tsx
+++ src/commands/new/templates/nft-app/app/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import './polyfills';
-import './app.css';
+import './styles.css';
 
 async function bootstrap() {
   const [{ default: App }, { AppProviders }] = await Promise.all([
