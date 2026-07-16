23c4732ef28adca5db7b10f042f77ef52f7b82b0
Danil Ovchinnikov <danil42worker@gmail.com>
2026-07-17 02:18:34 +0700

    fix `noUndeclaredDependencies`

--- crates/acton-localnet-ui/src/explorer/retrace/tasm/lib/types.ts
+++ crates/acton-localnet-ui/src/explorer/retrace/tasm/lib/types.ts
@@ -1,4 +1,4 @@
-import tvmSpecData from "@tasm-spec/tvm-specification.json"
+import tvmSpecData from "../../../../../../tasm-core/spec/tvm-specification.json"
 
 import type {FiftInstruction, Instruction, Specification} from "./specification"
 
