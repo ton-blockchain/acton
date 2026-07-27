60c6ca2d978278c28a33b6ff3b0b608792e003ef
Danil Ovchinnikov <danil42worker@gmail.com>
2026-07-28 00:17:59 +0700

    14

--- packages/address-registry/tests/generated.test.ts
+++ packages/address-registry/tests/generated.test.ts
@@ -1,6 +1,11 @@
 import {expect, test} from "bun:test"
 
+import addressesJson from "../src/addresses.json" with {type: "json"}
 import {addresses} from "../src/addresses.ts"
+
+test("TypeScript binding exposes addresses.json", () => {
+  expect(addresses).toEqual(addressesJson)
+})
 
 test("generated addresses are unique and sorted by raw address", () => {
   expect(addresses.length).toBeGreaterThan(0)
