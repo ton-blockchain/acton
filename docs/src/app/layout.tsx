039c77e42083218df5f08c719356e36ce3418d5f
Danil Ovchinnikov <danil42worker@gmail.com>
2026-09-16 15:02:48 +0400

    chore(docs): add favicon icons

--- docs/src/app/layout.tsx
+++ docs/src/app/layout.tsx
@@ -4,23 +4,28 @@ import {Provider} from "./provider"
 import {GeistSans} from "geist/font/sans"
 import {GeistMono} from "geist/font/mono"
 import "./globals.css"
-import logoDark from "@/public/logo-dark.svg"
-import logoLight from "@/public/logo-light.svg"
+import {baseUrl} from "@/lib/metadata"
 
 export const metadata: Metadata = {
+  manifest: `${baseUrl}/site.webmanifest`,
   icons: {
     icon: [
       {
-        url: logoLight.src,
-        media: "(prefers-color-scheme: light)",
-        type: "image/svg+xml",
+        url: `${baseUrl}/favicon-96x96.png`,
+        sizes: "96x96",
+        type: "image/png",
       },
       {
-        url: logoDark.src,
-        media: "(prefers-color-scheme: dark)",
+        url: `${baseUrl}/favicon.svg`,
         type: "image/svg+xml",
       },
     ],
+    shortcut: `${baseUrl}/favicon.ico`,
+    apple: {
+      url: `${baseUrl}/apple-touch-icon.png`,
+      sizes: "180x180",
+      type: "image/png",
+    },
   },
 }
 
